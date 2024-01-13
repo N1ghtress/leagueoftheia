e09902f0577ac22ac5a03a1a2a6680046dd02681
Faure Victor <faurevic+github@gmail.com>
2024-01-13 15:23:53 +0100

    Improved bar chart looks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const width = window.innerWidth
 const height = window.innerHeight
 const dir = 'data/'
 
-let svg = d3.select('body').append('svg')
+let svg = d3.select('div').append('svg')
 
 champion_mastery = d3.json(dir + 'champion_mastery_Bmn5dAH2VEy0fvqIHRrHtIV1-4iJd5hlW-R9DOSR7Ds5hR07ZgAtdGyeE5MQmVuY0A_9y2g_ygHscA.json')
 champion = d3.json(dir + 'champion_14.1.1.json')
@@ -29,32 +29,46 @@ function prepareData(champion, masteries) {
 }
 
 function drawBars(masteries) {
-    const color = d3.scaleLinear()
-        .range(["red", "blue"])
-        .domain([
-            d3.min(masteries, d => d.championPoints),
-            d3.max(masteries, d => d.championPoints)
-        ])
-    
-    // Size of a single data
-    const xSize = d3.scaleLinear()
-        .range([0, width])
-        .domain([
-            d3.min(masteries, d => d.championPoints),
-            d3.max(masteries, d => d.championPoints)
-        ])
-    const ySize = 30
+    pointSum = masteries.reduce((acc, cur) => acc + cur.championPoints, 0)
+    console.log(pointSum)
 
     // Size of svg
-    svg.attr("width", width * 0.8)
-        .attr("height", masteries.length * ySize)
+    svgWidth = width * 0.8
+    svgHeight = 600
+    svg.attr("width", svgWidth)
+        .attr("height", svgHeight)
         .style("margin", "auto")
 
-    bars = svg.selectAll("rect").data(masteries)
-    bars.join("rect")
-        .attr("y", (d, i) => ySize * i)
-        .attr("width", (d) => xSize(d.championPoints))
-        .attr("height", ySize)
-        .style("fill", (d) => color(d.championPoints))
-    return bars
+    let nextY = 0
+    const computeHeight = (d) => d.championPoints / pointSum * svgHeight
+    bar = svg.selectAll("g")
+        .data(masteries)
+        .enter()
+        .append("g")
+        .attr("transform", (d,i) => {
+            let height = computeHeight(d)
+            let y = nextY
+            nextY += height
+            return "translate(0," + y + ")" 
+        })
+
+    sepSize = 1
+    bar.append("rect")
+        .attr("width", svgWidth)
+        .attr("height", d => computeHeight(d) - sepSize)
+        .style("fill", "#ccc")
+
+    bar.append("rect")
+        .attr("width", svgWidth)
+        .attr("height", 1)
+        .attr("y", d => computeHeight(d) - sepSize)
+        .style("fill", "#000")
+
+    bar.append("text")
+        .attr("y", (d) => computeHeight(d) / 2 + 6)
+        .text(d => {
+            return d.championName
+        })
+    
+    return bar
 }
