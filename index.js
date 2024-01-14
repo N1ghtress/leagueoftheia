1d52c71f3946b91f17339afef14abba1c97a9f1a
Faure Victor <faurevic+github@gmail.com>
2024-01-14 18:35:29 +0100

    Completed icicle viz
    Displaying all 3 columns (total, class and champion) with separators

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ champion = d3.json(dir + 'champion_14.1.1.json')
 Promise.all([champion_mastery, champion]).then((results) => {
     let masteries = results[0]
     let champion = results[1]
-    console.log(champion, masteries)
-
     masteries = prepareData(champion, masteries)
-    bars = drawBars(masteries)
+    drawBars(masteries)
 })
 
 function prepareData(champion, masteries) {
@@ -24,51 +22,130 @@ function prepareData(champion, masteries) {
     masteries.forEach((m) => {
         m.championName = idNameMap[m.championId]
         m.championImage = idNameMap[m.championId] + '.png'
+        m.championTag = champion.data[idNameMap[m.championId]].tags[0]
     })
     return masteries
 }
 
 function drawBars(masteries) {
-    pointSum = masteries.reduce((acc, cur) => acc + cur.championPoints, 0)
-    console.log(pointSum)
+    totalMastery = [{
+        championPoints: masteries.reduce((a, c) => a + c.championPoints, 0)
+    }]
+
+    classMasteries = masteries.reduce((a, c) => {
+        if (!a[c.championTag]) {
+            a[c.championTag] = {}
+            a[c.championTag].championTag = c.championTag
+            a[c.championTag].championPoints = 0
+        }
+        a[c.championTag].championPoints += c.championPoints
+        return a
+    }, {})
+    classMasteries['Support'].color = '#1f77b4',
+    classMasteries['Assassin'].color = '#ff7f0e',
+    classMasteries['Tank'].color = '#2ca02c',
+    classMasteries['Fighter'].color = '#d62728',
+    classMasteries['Mage'].color = '#9467bd',
+    classMasteries['Marksman'].color = '#8c984b',
+    classMasteriesArray = []
+    for (tag in classMasteries) {
+        classMasteriesArray.push(classMasteries[tag])
+    }
+    masteries.sort((a, b) => classMasteries[a.championTag].championPoints < classMasteries[b.championTag].championPoints)
 
     // Size of svg
-    svgWidth = width * 0.8
-    svgHeight = 600
-    svg.attr("width", svgWidth)
-        .attr("height", svgHeight)
+    SVG_WIDTH = width * 0.8
+    SVG_HEIGHT = 750
+    svg.attr("width", SVG_WIDTH)
+        .attr("height", SVG_HEIGHT)
         .style("margin", "auto")
+    BAR_WIDTH = SVG_WIDTH / 3
+    FONT_SIZE = 14
+    TOP_TEXT_X_OFFSET = 0
+    TOP_TEXT_Y_OFFSET = 0
 
     let nextY = 0
-    const computeHeight = (d) => d.championPoints / pointSum * svgHeight
-    bar = svg.selectAll("g")
+    const computeHeight = (d) => {
+        return d.championPoints / totalMastery[0].championPoints * SVG_HEIGHT
+    }
+    sepSize = 1
+
+    // Total
+    totalGroup = svg.append("g")
+    totalBar = totalGroup.data(totalMastery)
+        .append("rect")
+        .attr("width", BAR_WIDTH)
+        .attr("height", SVG_HEIGHT)
+        .style("font-size", FONT_SIZE + "px")
+        .style("fill", "#ccc")
+    totalText = totalGroup.data(totalMastery)
+        .append("text")
+        .attr("y", 21)
+        .text(d => "Total " + d.championPoints)
+
+    // Class
+    classGroup = svg.append("g")
+    classBar = classGroup.selectAll("g")
+        .data(classMasteriesArray)
+        .enter()
+        .append("g")
+        .style("font-size", FONT_SIZE + "px")
+        .attr("transform", d => {
+            let y = nextY
+            nextY += computeHeight(d)
+            return "translate(0, " + y + ")"
+        })
+    classBar.append("rect")
+        .attr("x", BAR_WIDTH)
+        .attr("width", BAR_WIDTH)
+        .attr("height", d => {
+            return computeHeight(d) - sepSize            
+        })
+        .style("fill", d => d.color)
+    classBar.append("rect")
+        .attr("x", BAR_WIDTH)
+        .attr("y", d => computeHeight(d) - sepSize)
+        .attr("width", BAR_WIDTH)
+        .attr("height", sepSize)
+    classBar.append("text")
+        .attr("x", BAR_WIDTH)
+        .attr("y", (d) => 21)
+        .text(d => {
+            if (computeHeight(d) < 14) return ''
+            return d.championTag + ' ' + d.championPoints
+        })
+    
+    // Champions
+    nextY = 0
+    championGroup = svg.append("g")
+    championBar = championGroup.selectAll("g")
         .data(masteries)
         .enter()
         .append("g")
-        .attr("transform", (d,i) => {
-            let height = computeHeight(d)
+        .style("font-size", FONT_SIZE + "px")
+        .attr("transform", d => {
             let y = nextY
-            nextY += height
+            nextY += computeHeight(d)
             return "translate(0," + y + ")" 
         })
-
-    sepSize = 1
-    bar.append("rect")
-        .attr("width", svgWidth)
-        .attr("height", d => computeHeight(d) - sepSize)
-        .style("fill", "#ccc")
-
-    bar.append("rect")
-        .attr("width", svgWidth)
-        .attr("height", 1)
+    championBar.append("rect")
+        .attr("x", BAR_WIDTH * 2)
+        .attr("width", BAR_WIDTH)
+        .attr("height", d => { 
+            return computeHeight(d) - sepSize
+        })
+        .style("fill", d => classMasteries[d.championTag].color)
+    championBar.append("rect")
+        .attr("x", BAR_WIDTH * 2)
+        .attr("width", BAR_WIDTH)
+        .attr("height", sepSize)
         .attr("y", d => computeHeight(d) - sepSize)
         .style("fill", "#000")
-
-    bar.append("text")
-        .attr("y", (d) => computeHeight(d) / 2 + 6)
+    championBar.append("text")
+        .attr("x", BAR_WIDTH * 2)
+        .attr("y", (d) => computeHeight(d) / 2 + 7)
         .text(d => {
-            return d.championName
+            if (computeHeight(d) < 14) return ''
+            return d.championName + ' ' + d.championPoints
         })
-    
-    return bar
 }
